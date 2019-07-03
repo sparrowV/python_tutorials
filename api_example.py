75cee2f356c157f75f525fbc3cf1002600688df7
oto <odval14@freeuni.edu.ge>
2019-07-04 00:00:24 +0400

    tutorial 2

--- api_example.py
+++ api_example.py
@@ -3,11 +3,17 @@ import requests
 def getSummary(users,params):
     result = {}
     for user in users:
-        result[user['id']] =user['name']+" , "+user['phone']
+        paramValueList = []
+        for paramName in params:
+            paramValueList.append(user[paramName])
+        result[user['id']] =paramValueList
     return result
 
 response = requests.get("https://jsonplaceholder.typicode.com/users",timeout = 3)
 response_data = response.json()
 print(len(response_data))
-summary = getSummary(response_data)
-print(summary)
+if(response.ok):
+    summary = getSummary(response_data,['name','email','phone','website'])
+    print(summary)
+else:
+    print("some error happend",response.status_code)
